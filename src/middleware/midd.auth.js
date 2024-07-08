6ac95bbccdad4f24b1f1ebdd9a74561c93241d46
godie37 <godie37@gmail.com>
2024-07-07 23:55:04 -0300

    cambio sessioKey

--- src/middleware/midd.auth.js
+++ src/middleware/midd.auth.js
@@ -2,7 +2,7 @@ import session from 'express-session';
 import 'dotenv/config';
 
 export const authSession = session({
-    secret: process.env.SESSION_KEY,
+    secret: 'hola123',
     resave: false,
     saveUninitialized: false,
     expires: new Date(Date.now() + 2629800000)
